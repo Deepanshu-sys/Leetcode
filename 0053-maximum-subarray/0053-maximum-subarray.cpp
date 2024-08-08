1179e3b0cad392f93b1c13ecc83419663e8fe8ae
DEEPANSHU KUMAR <150465574+Deepanshu-sys@users.noreply.github.com>
2024-08-08 17:58:49 +0530

    Time: 76 ms (62.32%), Space: 70.6 MB (37.49%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -3,7 +3,7 @@ public:
     int maxSubArray(vector<int>& nums) {
         int sum = nums[0];
         int curr = nums[0];
-
+        // its kadens algorinthm if you forgot revise;
         for(int i = 1;i < nums.size();i++)
         {
             curr = max(nums[i],curr + nums[i]);
